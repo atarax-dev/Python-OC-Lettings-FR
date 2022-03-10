83a92c2e61a98fff41b9602d1740773482d5ae01
atarax-dev <83333973+atarax-dev@users.noreply.github.com>
2022-03-10 01:46:05 +0100

    Refonte architecture
    add apps lettings et profiles
    urls modifiées et vues transferées
    templates mis a jour

--- lettings/models.py
+++ lettings/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinLengthValidator
-from django.contrib.auth.models import User
 
 
 class Address(models.Model):
@@ -21,15 +20,7 @@ class Address(models.Model):
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, related_name='+')
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='+')
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
